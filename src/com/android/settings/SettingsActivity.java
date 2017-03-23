2e3ebeb4fb6ff23f9a3fb3f495467a56d5561d29
theringmaster <gzroms@hotmail.com>
2017-03-23 18:23:21 -0400

    Add Kernel Adiutor to dashboard
    Gzr vector

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -165,6 +165,7 @@ public class SettingsActivity extends SettingsDrawerActivity
     private static final String SAVE_KEY_SHOW_HOME_AS_UP = ":settings:show_home_as_up";
     private static final String SAVE_KEY_SHOW_SEARCH = ":settings:show_search";
     private static final String SAVE_KEY_HOME_ACTIVITIES_COUNT = ":settings:home_activities_count";
+    private static final String KA_FRAGMENT = "com.android.settings.ka";
 
     /**
      * When starting this activity, the invoking Intent can contain this extra
@@ -1062,6 +1063,13 @@ public class SettingsActivity extends SettingsDrawerActivity
             finish();
             return null;
         }
+        if (KA_FRAGMENT.equals(fragmentName)) {
+            Intent kaIntent = new Intent();
+            kaIntent.setClassName("com.grarak.kerneladiutor", "com.grarak.kerneladiutor.MainActivity");
+            startActivity(kaIntent);
+            finish();
+            return null;
+        }
         if (validate && !isValidFragment(fragmentName)) {
             throw new IllegalArgumentException("Invalid fragment for this activity: "
                     + fragmentName);
@@ -1171,6 +1179,16 @@ public class SettingsActivity extends SettingsDrawerActivity
 
         // Reveal development-only quick settings tiles
         DevelopmentTiles.setTilesEnabled(this, showDev);
+
+        // Kernel Adiutor
+        boolean kapresent = false;
+        try {
+            kapresent = (getPackageManager().getPackageInfo("com.grarak.kerneladiutor", 0).versionCode > 0);
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+        setTileEnabled(new ComponentName(packageName,
+                        Settings.KActivity.class.getName()),
+                kapresent, isAdmin, pm);
 
         // Magisk Manager
         boolean magiskSupported = false;
