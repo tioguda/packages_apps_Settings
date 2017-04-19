nitrogen-project/android_packages_apps_Settings

DUI: Initial N checkin

src/com/android/settings/SettingsActivity.java
@@ -130,6 +130,11 @@ import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 import com.nitrogen.settings.NitrogenSettings;
 
+import com.nitrogen.settings.fragments.FlingSettings;
+import com.nitrogen.settings.fragments.NavbarSettings;
+import com.nitrogen.settings.fragments.SmartbarSettings;
+import com.nitrogen.settings.fragments.PulseSettings;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -350,7 +355,11 @@ public class SettingsActivity extends SettingsDrawerActivity
             TestingSettings.class.getName(),
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
 

tioguda/packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -135,6 +135,10 @@ import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 
 import com.gzr.teslacoil.TeslaCoil;
+import com.gzr.teslacoil.fragments.FlingSettings;
+import com.gzr.teslacoil.fragments.NavbarSettings;
+import com.gzr.teslacoil.fragments.SmartbarSettings;
+import com.gzr.teslacoil.fragments.PulseSettings;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -362,6 +366,10 @@ public class SettingsActivity extends SettingsDrawerActivity
             AutomaticStorageManagerSettings.class.getName()
             TeslaCoil.class.getName(),
             LightsSettings.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
 